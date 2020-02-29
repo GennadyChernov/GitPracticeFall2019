5c4fd37719f3aeffe1782cad28fe100e57251923
GennadyChernov <58498342+GennadyChernov@users.noreply.github.com>
2020-02-29 15:51:54 -0500

    newUpdate

--- src/day1/LoopPractise.java
+++ src/day1/LoopPractise.java
@@ -5,7 +5,7 @@ public class LoopPractise {
         for (int i = 0; i < 10; i += 2) {
             System.out.println("counter : " + i);
         }
-        System.out.println("#####");
+        System.out.println("######");
 
     }
 

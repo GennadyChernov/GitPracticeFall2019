770fed1bb069a04ddbc64b50466be013a73c2b98
GennadyChernov <58498342+GennadyChernov@users.noreply.github.com>
2020-02-29 15:45:26 -0500

    LoopPractice

--- src/LoopPractise.java
+++ src/LoopPractise.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class LoopPractise {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println("counter : " + i);
+        }
+    }
 }
